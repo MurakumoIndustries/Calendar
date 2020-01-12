faea21197394cab42e23e668a0c361da73d44b92
WindWT <leibao0723@gmail.com>
2020-01-12 17:04:38 +0800

    style fix

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="modal fade" tabindex="-1" role="dialog">
         <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-            <div class="modal-content">
+            <div class="modal-content background">
                 <div class="row">
                     <div class="col-12 col-lg-auto text-center">
                         <div class="chara-img-container">
